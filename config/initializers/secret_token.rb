28370edc882d4413e70da7b661e1b73a2981133b
nsbarrcanvas <nick@canv.as>
2012-10-19 14:35:38 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '03c781df74c4835f6351380489887c57bcdff8b99d1361543c28bb264fd20daa19dfb306053a7f28a1864be09cf6069e30707cc5b2308cdec5636178b330057d'
