35d6ba438891bab8033b97239b5f57821de7340c
nsbarrcanvas <nick@canv.as>
2012-10-19 20:29:08 -0400

    Made a form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 FirstApp::Application.routes.draw do
+  get "users/new"
+  
+  root to: 'static_pages#home'
+
+    match '/signup',  to: 'public#index'
+
+    match '/help',    to: 'static_pages#help'
+    match '/about',   to: 'static_pages#about'
+    match '/contact', to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
