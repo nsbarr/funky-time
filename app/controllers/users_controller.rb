e4d2cb69cf1966f321c909f5b61ee4cac0da8c32
nsbarrcanvas <nick@canv.as>
2012-11-09 17:29:27 -0500

    fixed repeat twilio bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,6 @@ require 'twilio-ruby'
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    #begin twilio bullshit
-    #@number_to_call = User.phone
-    #end twilio bullshit
-    number_to_send_to = @user.phone
-    theme = @user.prompt
-
-     twilio_sid = "ACfff561dd3ac397a29183f7bf7d68e370"
-     twilio_token = "cbb3471db9d83b61598159b5210404f1"
-     twilio_phone_number = "6464900303"
-
-     @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
-
-     @twilio_client.account.sms.messages.create(
-       :from => "+1#{twilio_phone_number}",
-       :to => number_to_send_to,
-       :body => "Thanks, I'm dreaming up your poem about #{theme}"
-     )
   end
   
   def userlog
@@ -32,6 +15,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      #begin twilio bullshit
+      #@number_to_call = User.phone
+      #end twilio bullshit
+      number_to_send_to = @user.phone
+      theme = @user.prompt
+
+       twilio_sid = "ACfff561dd3ac397a29183f7bf7d68e370"
+       twilio_token = "cbb3471db9d83b61598159b5210404f1"
+       twilio_phone_number = "6464900303"
+
+       @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+       @twilio_client.account.sms.messages.create(
+         :from => "+1#{twilio_phone_number}",
+         :to => number_to_send_to,
+         :body => "Thanks, I'm dreaming up your poem about #{theme}"
+       )
 # noflash      flash[:success] = %Q[Thanks, I'll be texting you soon!]
      redirect_to @user
     else
