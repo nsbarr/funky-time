3c0e6f0cb44c5375b44d433858b2d37298bd84bf
nsbarrcanvas <nick@canv.as>
2012-11-12 15:43:17 -0500

    comment out smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,15 @@
 FirstApp::Application.configure do
   
- config.action_mailer.raise_delivery_errors = true
+# config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 25,
-    :user_name            => 'nsbarr@gmail.com',
-    :password             => 'mchlwn4uk',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+#  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.smtp_settings = {
+#    :address              => "smtp.gmail.com",
+#    :port                 => 25,
+#    :user_name            => 'nsbarr@gmail.com',
+#    :password             => 'mchlwn4uk',
+#    :authentication       => 'plain',
+#    :enable_starttls_auto => true  }
     
     
   # Settings specified here will take precedence over those in config/application.rb
