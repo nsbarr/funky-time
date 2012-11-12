ae808a5f25ccaec148f919db69f2813552a387c4
nsbarrcanvas <nick@canv.as>
2012-11-12 15:38:11 -0500

    change port last try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 FirstApp::Application.configure do
   
-  config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    :port                 => 587,
+    :port                 => 25,
     :user_name            => 'nsbarr@gmail.com',
     :password             => 'mchlwn4uk',
     :authentication       => 'plain',
