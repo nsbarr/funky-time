6e990abbb40387818e94bbe5e056674cb72476b5
nsbarrcanvas <nick@canv.as>
2012-11-12 19:00:50 -0500

    fixed styling

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+
 module FirstApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -41,6 +42,8 @@ module FirstApp
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
+    
+    
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
